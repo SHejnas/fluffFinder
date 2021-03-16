31299f4f9462c5df44a419efd05dcb2b05409235
Steven Hejnas <49131468+SHejnas@users.noreply.github.com>
2021-03-16 14:28:47 -0400

    first commit

---
+++ src/main/java/com/stevenhejnas/flufffinder/FluffFinderApplication.java
@@ -0,0 +1,13 @@
+package com.stevenhejnas.flufffinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stevenhejnas/flufffinder/FluffFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevenhejnas.flufffinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
